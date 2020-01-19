ffa83c6b63e5a1e97a3675fb8bd294697e0ce1a8
Administrator <@denglinjie>
2020-01-19 17:12:12 +0800

    scroll

--- src/views/layouts/Index.vue
+++ src/views/layouts/Index.vue
@@ -1,122 +1,40 @@
 <template>
     <v-app app>
-
-        <v-navigation-drawer
-                app
-                fixed
-                width="200"
-                v-model="primaryDrawer.model"
-                :clipped="primaryDrawer.clipped"
-                :floating="primaryDrawer.floating"
-                :mini-variant="primaryDrawer.mini"
-                :permanent="primaryDrawer.type === 'permanent'"
-                :temporary="primaryDrawer.type === 'temporary'"
-                overflow
-        >
-
-            <v-toolbar elevation="0">
-                <v-btn icon>
-                    <v-img contain src="https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png" width="32px" height="32"/>
-                </v-btn>
-
-                <v-toolbar-title >
-                    微人事
-                </v-toolbar-title>
-            </v-toolbar>
-
-            <LeftNavigation></LeftNavigation>
-        </v-navigation-drawer>
-
-
-
-        <v-app-bar
-          :clipped-left="primaryDrawer.clipped"
-          app
-          elevate-on-scroll
-          hide-on-scroll
-        >
-            <v-app-bar-nav-icon
-                v-if="primaryDrawer.model"
-                @click.stop="primaryDrawer.mini = !primaryDrawer.mini"
-            />
-
-            <v-toolbar-title v-if="!primaryDrawer.model">微人事</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-
-
-<!--            <HorizontalNavigation v-if="this.primaryDrawer.clipped"></HorizontalNavigation>-->
-
-
-            <!--头像-->
-            <Avatar @layoutNavClipped="layoutNavClipped"/>
-
-            <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-
-
-
-            <template v-slot:extension v-if="primaryDrawer.clipped">
-                <HorizontalNavigation></HorizontalNavigation>
-            </template>
-
-        </v-app-bar>
-
-
-        <v-content>
-            <v-container fluid>
-                <router-view />
-            </v-container>
-        </v-content>
-
+     <v-content>
+        <v-container >
+            <v-app-bar
+                    color="indigo darken-2"
+                    dark
+                    absolute
+            >
+                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                    <v-toolbar-title>Title</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                        <v-icon>mdi-magnify</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                        <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+            </v-app-bar>
+        </v-container>
+     </v-content>
     </v-app>
 </template>
 
+
 <script>
-import Avatar from './Avatar.vue'
-import LeftNavigation from './LeftNavigation.vue'
-import HorizontalNavigation from './HorizontalNavigation.vue'
-
 export default {
+    name: 'Index_',
     data: () => ({
-        //  默认 permanent（不能隐藏或显示为true不能影藏）  Temporary（层叠模式）
-        drawers: ['Default (no property)', 'Permanent', 'Temporary'],
-        primaryDrawer: {
-            model: false,  // 控制左侧菜单是否显示
-            type: 'Permanent', //
-            clipped: true,  // 顶部导航是否是通航
-            floating: true, // 是否影藏边框
-            mini: true,  // 是否用模拟模式
-        },
-        footer: {
-            inset: true,
-        },
-    }),
-
-    watch: {
-        primaryDrawer: {
-            handler: function (val) {
-                console.log(val)
-            },
-            immediate: true,
-            deep: true
-        }
-    },
-
-    components:{
-        Avatar, LeftNavigation, HorizontalNavigation
-    },
-
-    methods:{
-        layoutNavClipped(val){
-            this.primaryDrawer.clipped = val
-            this.primaryDrawer.model = !val
-        }
-    },
-    mounted () {
-        this.$Progress.start()
-    }
-
+        model:''
+    })
 }
 </script>
+
+<style rel="stylesheet/sass" lang="sass" scoped>
+
+</style>
